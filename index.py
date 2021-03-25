d9a88ffbfea616e4946e4e10266b2857ca9fa064
고찬현 <rhcksgus2@gmail.com>
2021-03-25 02:13:18 +0000

    그래프 그리기

--- index.py
+++ index.py
@@ -1,7 +1,13 @@
-import requests;
+import xmltodict
+import requests
+import json
+import matplotlib.pyplot as plt
 
 url = "http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty"
-queryParams = "?ServiceKey=서비스키&numOfRows=10&pageNo=1&stationName=종로구&dataTerm=DAILY&ver=1.3"
+queryParams = "?ServiceKey=hJ1gcOgUq0k%2F2DLu0BVpkgVelUaGFGr0RiT7%2Br94vpTWGvNDiVn390MvVkTH0ZkwjDuBymy7m1t%2FyB5nP5E5lQ%3D%3D&numOfRows=10&pageNo=1&stationName=한솔동&dataTerm=DAILY&ver=1.3"
 
-request = requests.get(url + queryParams)
-print(request.text)
+res = requests.get(url + queryParams)
+cc = xmltodict.parse(res.text)
+data = json.loads(json.dumps(cc))
+plt.plot(data['response']['body']['items']['item'][0]['o3Value'], data['response']['body']['items']['item'][1]['o3Value'])
+plt.show()
